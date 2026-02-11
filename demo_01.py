14cd2a1da649382c63fb5b6804b11317285dbd4c
YanHuiYong <410889472@qq.com>
2026-02-11 10:03:29 +0800

    sync pypi

---
+++ demo_01.py
@@ -0,0 +1,10 @@
+# Time: 2026/2/10 15:40         
+# Author: 颜慧雍                   
+# File: demo_01.py             
+# Software: PyCharm
+
+from yan_logger import MyLogger
+
+ml = MyLogger()
+
+ml.info('info')
